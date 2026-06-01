8eab7117a2a144b1741fe961c4f209eeafef9ebc
jbgordonNU <f4f94m65wn@privaterelay.appleid.com>
2026-06-01 13:40:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import time
 import requests
 import pandas as pd
 import streamlit as st
-from rdkit.Chem import AllChem
-from rdkit import Chem
 from streamlit_ketcher import st_ketcher
 
 st.set_page_config(page_title="Chemical Substructure Locator", layout="wide")
